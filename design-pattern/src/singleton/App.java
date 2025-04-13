71959f8d434053008794cb24e96318ec8d8fd556
Chambit Oh <chambit.oh@gmail.com>
2025-04-13 12:28:03 +0300

    singleton

--- design-pattern/src/singleton/App.java
+++ design-pattern/src/singleton/App.java
@@ -2,11 +2,11 @@ package singleton;
 
 public class App {
     public static void main(String[] args) {
-        Settings settings1 = Settings.getInstance();
+        Settings settings = Settings.getInstance();
         Settings settings2 = Settings.getInstance();
 
-        System.out.println(settings1 == settings2); // true
-        System.out.println(settings1.hashCode());
+        System.out.println(settings == settings2); // true
+        System.out.println(settings.hashCode());
         System.out.println(settings2.hashCode());
     }
 }
